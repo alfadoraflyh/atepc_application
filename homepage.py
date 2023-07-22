1929bf49e2a47280a600199f008393ca1b6005c6
alfadoraflyh <72862438+alfadoraflyh@users.noreply.github.com>
2023-07-22 20:57:43 +0700

    last version of application

--- homepage.py
+++ homepage.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from io import StringIO
 from PIL import Image
+import random
+import json
 from predict_atepc import Predict_ATEPC
 
 def analyze_text(input_text_form):
@@ -8,7 +10,6 @@ def analyze_text(input_text_form):
     ATEPC.predict_single_text(input_text_form)
     ATEPC.list_into_dataframe()
     return ATEPC.dataframe_predict
-
 
 def analyze_file(input_file_form):
     ATEPC = Predict_ATEPC()
@@ -20,14 +21,18 @@ def analyze_file(input_file_form):
     ATEPC.make_bar_chart(type_of_sentiments='negative', palette_color='rocket')
     display_results_file(ATEPC.dataframe_predict_texts)
 
-def display_results_text(results_df, input_form):
-    if results_df.empty:
-        st.error("Sorry the model is still development ☹️")
+def display_results_text(results_df, input_form, selected_image):
+    if results_df is None or results_df.empty:
+        st.error("Sorry the model is still in development ☹️")
     else:
         st.success("Aspect Found!")
         st.subheader('Aspect-Based Sentiment Analysis Results')
-        st.markdown(f"Sentence : {input_form}")
+        st.markdown(f"Sentence: {input_form}")
         st.dataframe(results_df, hide_index=True)
+
+    if selected_image is not None:
+        image_location = Image.open(f'photo/{selected_image}.jpg')
+        st.image(image_location, caption=f'{selected_image}')
 
 def display_results_file(dataframe):
     st.success("Aspect Found!")
@@ -40,15 +45,62 @@ def display_results_file(dataframe):
     st.image(image_barchart_positive, caption='Top Aspek Positif')
     st.image(image_barchart_negative, caption='Top Aspek Negatif')
 
+def choose_random_sentence(input_choose):
+    with open('datasets/top 10 destination aceh.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    # Extract the relevant comments
+    extracted_comment = []
+    for item in data:
+        place_name = item['place_name']
+        if place_name == f'{input_choose}':
+            comments = item['comment']
+            extracted_comment.extend(comments)
+    sentence = random.choice(extracted_comment)
+    sentence = sentence.strip()
+    return sentence
+def clean_input():
+    # Code to clean input text
+    st.text_input("Enter text here", value="")
+
 
 def main():
-    st.title('Aspect-Based Sentiment Analysis App')
-    st.write('Enter your text below:')
-    input_text = st.text_area('Input Text')
+    st.set_page_config(
+        page_title="ABSA Aceh Tourism App" ,
+        page_icon="🕌",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'About': 'https://www.extremelycoolapp.com/help',
+        }
+    )
+    header_image = Image.open('photo/header_image.jpg')
+    st.image(header_image)
+    st.title('Aspect-Based Sentiment Analysis for Aceh Tourism')
 
-    if st.button('Analyze Text'):
-        results_df = analyze_text(input_text)
-        display_results_text(results_df, input_text)
+    st.write('Enter your text by yourself:')
+    input_text_area = st.text_area('Input Text')
+    st.write('Enter a text-based review on the ten most visited tourism places in Aceh:')
+    selected_text = st.selectbox(
+        '',
+        ('Monument 0 km Indonesia', 'Aceh State Museum', 'Boat at Desa Lampulo',
+         'Baiturrahman Grand Mosque', 'Rubiah Island','Mie Razali', 'Freddies Santai Sumurtiga',
+         'Mahi Mahi Surf Resort', 'Iboih Beach', 'Aceh Tsunami Museum'), label_visibility="collapsed")
+
+    st.write('You selected:', selected_text)
+
+    # Radio Button
+    if selected_text:
+        input_text_radio = choose_random_sentence(selected_text)
+
+    with st.spinner('Please Wait...'):
+        if st.button('Analyze Text'):
+            if len(input_text_area) > 0:
+                selected_text = None
+                results_df = analyze_text(input_text_area)
+                display_results_text(results_df, input_text_area, selected_text)
+
+            else:
+                results_df = analyze_text(input_text_radio)
+                display_results_text(results_df, input_text_radio, selected_text)
 
     st.write('---')
     st.write('Upload a file for analysis:')
@@ -61,8 +113,6 @@ def main():
             string_data = stringio.read()
             sentences = string_data.split('\n')
             analyze_file(sentences)
-
-
 
 if __name__ == '__main__':
     main()
